72ed520abb98ff51a475a9682d66a3f753a40d02
Joshua Comeau <joshwcomeau@gmail.com>
2017-02-23 07:51:48 -0500

    Disable outdated unit test for nonexistent component

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Hello from 'client/components/Hello';
 
-describe('Hello.vue', () => {
+describe.skip('Hello.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Hello);
     const vm = new Constructor().$mount();
