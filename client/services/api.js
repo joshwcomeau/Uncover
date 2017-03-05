494b3ae1cfd7bfa241a376589906b9f031dd7e35
Joshua Comeau <joshwcomeau@gmail.com>
2017-03-05 09:30:05 -0500

    Re-implement API logic in Express

--- client/services/api.js
+++ client/services/api.js
@@ -1,19 +1,17 @@
 const apiHost = `${process.env.HOST}:${process.env.PORT}`;
 
-console.log(`${process.env.HOST}:${process.env.PORT}`)
-
 const toJson = response => response.json();
 
 
 export const getTrackInfo = ({ searchTerm, category }) => {
-  const path = `${apiHost}/get-track-info`;
+  const path = `${apiHost}/track-info`;
   const query = `searchTerm=${searchTerm}&category=${category}`;
 
   return fetch(`${path}?${query}`).then(toJson);
 };
 
 export const getTrackItems = (track) => {
-  const url = `${apiHost}/get-track-items`;
+  const url = `${apiHost}/track-items`;
 
   // Doing a POST because we're sending a lot of info for a GET.
   // Plus, in the future, this may also save the current 'snapshot' and return
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const {
   getTrackItems,
@@ -14,14 +15,39 @@ const app = express();
 const rootDir = path.join(__dirname, '..');
 
 
+////////////////////////////////
+///////// MIDDLEWARES /////////
+/////////////////////////////
+app.use(bodyParser.json());
+
+// This middleware serves all static assets, INCLUDING our main index.html,
+// when the root route is hit ('/').
 app.use(express.static('client/dist'));
 
-app.get('/', (req, res) => {
-  console.log(rootDir, path.resolve(rootDir, 'dist/index.html'))
-  return res.sendFile(path.resolve(rootDir, 'dist/index.html'));
+
+///////////////////////////
+///////// ROUTES /////////
+/////////////////////////
+app.get('/track-info', (req, res) => {
+  getAuthorProfileAndTrackItems(req.query.searchTerm)
+    .then((results) => {
+      return res.json(results);
+    });
+});
+
+app.post('/track-items', (req, res) => {
+  const { track, options = {} } = req.body;
+
+  getTrackItems(track, options)
+    .then((results) => {
+      return res.json(results);
+    });
 });
 
 
+///////////////////////////
+///////// RUN ////////////
+/////////////////////////
 app.listen(config.PORT, () => {
   console.log('⚡️  Server listening on port ' + config.PORT + '.');
 });
